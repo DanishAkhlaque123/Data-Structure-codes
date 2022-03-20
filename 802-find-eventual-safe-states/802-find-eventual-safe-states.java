3ebf03032b6f757dfa8e3c23768ad0bc258e256e
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2022-03-20 21:49:04 +0530

    Time: 9 ms (57.61%), Space: 68 MB (62.68%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -0,0 +1,47 @@
+class Solution {
+         public boolean dfs(int src,int[][] graph, int[] vis)
+        {
+            vis[src]=1;
+            for(int nbr:graph[src])
+            {
+                if(vis[nbr]==0)
+                {
+                    boolean cycle=dfs(nbr,graph,vis);
+                if(cycle)
+                    return true;
+                
+                }
+                else if(vis[nbr]==1)
+                {
+                    return true;
+                }
+                
+            }
+            vis[src]=2;
+            return false;
+        }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+   
+        int v=graph.length;
+        int[] vis=new int[v];
+        for(int i=0;i<v;i++)
+        {
+            if(vis[i]==0)
+            {
+                boolean cycle=dfs(i,graph,vis);
+            }
+        }
+        
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<v;i++)
+        {
+            if(vis[i]==2)
+            {
+                ans.add(i);
+            }
+        }
+        return ans;
+        
+        
+    }
+}
