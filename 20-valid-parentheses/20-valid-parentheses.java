755ba35dc888ee80af66303823b5345d2c8907d0
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2022-03-26 14:13:43 +0530

    Time: 1 ms (99.43%), Space: 40.1 MB (87.57%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n=s.length();
+        if(n%2==1)
+            return false;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            char c=s.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+            {
+                st.push(c);
+            }
+            else 
+            {
+                if(st.size()==0)
+                {
+                    return false;
+                }
+                
+                else if(c==']' && st.pop()!='[')
+                    return false;
+                else if(c=='}' && st.pop()!='{')
+                    return false;
+                else if(c==')' && st.pop()!='(')
+                    return false;
+            }
+            
+        }
+        if(st.size()!=0)
+            return false;
+        return true;
+    }
+}
