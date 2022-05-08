9bccdf5a6a0f1bdd1c60dde56f3aa2d9620b371a
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2022-05-08 11:03:24 +0530

    Added solution - LeetHub

---
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            new Solution().segregate0and1(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    void swap(int[] arr,int i,int j)
+    {
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+    
+    void segregate0and1(int[] arr, int n) {
+        // code here
+        int p1=-1;
+        int p2=0;
+        while(p2<arr.length)
+        {
+            if(arr[p2]==1)
+            p2++;
+            else
+            {
+                p1++;
+                swap(arr,p1,p2);
+                p2++;
+            }
+        }
+        
+    }
+
+}
