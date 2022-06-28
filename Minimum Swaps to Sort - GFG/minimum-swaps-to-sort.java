2b5c08d4b2e8085fc41a759ba301b15850c5cfcf
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2022-06-28 19:50:40 +0530

    Added solution - LeetHub

---
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] nums = new int[n];
+            String[] S = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.minSwaps(nums);
+            System.out.println(ans);
+       }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to find the minimum number of swaps required to sort the array.
+    public int minSwaps(int nums[])
+    {
+        // Code here
+        int n=nums.length;
+        pair[] res=new pair[n];
+        for(int i=0;i<n;i++)
+        {
+            res[i]=new pair(nums[i],i);
+            
+        }
+        
+        Arrays.sort(res);
+        int ans=0;
+        boolean[] vis=new boolean[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==true || res[i].idx==i)
+            continue;
+            
+            int j=i;
+            int count=0;
+            while(vis[j]==false)
+            {
+                count++;
+                vis[j]=true;
+                j=res[j].idx;
+            }
+            
+            ans+=count-1;
+        }
+        return ans;
+        
+    }
+    
+     public class pair implements Comparable<pair> {
+        int val;
+        int idx;
+         public pair(int val,int idx)
+        {
+            this.val=val;
+            this.idx=idx;
+        }
+        
+        public int compareTo(pair o){
+            return this.val-o.val;
+        }
+
+     }
+}
+    
