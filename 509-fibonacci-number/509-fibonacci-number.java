09a10e5fb392c1f574730666733aa43f2464dd09
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2022-07-06 10:21:41 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (13.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,19 +1,15 @@
 class Solution {
     public int fib(int n) {
-        int[] dp=new int[n+1];
-        return fibdp(n,dp);
-        
-    }
-    
-    
-    public int fibdp(int n,int[] dp)
-    {
-        if(n==0 || n==1)
+
+        if(n<=1)
             return n;
-        if(dp[n]!=0)
-            return dp[n];
-        
-        dp[n]= fibdp(n-1,dp)+fibdp(n-2,dp);
-        return dp[n];
+        int[] ans=new int[n+1];
+        ans[0]=0;
+        ans[1]=1;
+        for(int i=2;i<n+1;i++)
+        {
+            ans[i]=ans[i-1]+ans[i-2];
+        }
+       return ans[n];
     }
 }
