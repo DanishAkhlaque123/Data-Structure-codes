8b578a5ee4d29f4ca1663498f50240bd43fa2183
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2022-07-15 09:26:21 +0530

    Time: 8 ms (15.10%), Space: 50.8 MB (7.32%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,37 +1,44 @@
 class Solution {
+   int count=0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int srow=0;
+        int erow=grid.length-1;
+        int scol=0;
+        int ecol=grid[0].length-1;
+        for(int i=0;i<=erow;i++)
+        {
+            for(int j=0;j<=ecol;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    
+                    int t=dfs(grid,i,j);
+                        count=Math.max(count,t);
+                    
+                 }
+            }
+        }
+        return count;
+    }
     
-    
-    public int dfs(int i,int j,int[][] grid)
+    public int dfs(int[][] grid, int i, int j)
     {
         grid[i][j]=0;
-        int size=0;
         int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
+         int ans=0;
         for(int[] dir:dirs)
         {
             int x=i+dir[0];
             int y=j+dir[1];
-            if(x>=0 && y>=0 && x<grid.length && y<grid[0].length && grid[x][y]==1)
+            if(x>=0 && x<=grid.length-1 && y>=0 && y<=grid[0].length-1 && grid[x][y]==1)
             {
-                size+=dfs(x,y,grid);  
+                
+               ans+= dfs(grid,x,y);
             }
+           
         }
-        return size+1;
+        return ans+1;
+        
     }
-    public int maxAreaOfIsland(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    int size=dfs(i,j,grid);
-                    ans=Math.max(ans,size);
-                }
-            }
-        }
-        return ans;
-    }
+    
 }
