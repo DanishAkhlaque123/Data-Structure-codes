5a816305c989a23ec45e3a96672ec9beb16dabbf
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2023-12-28 22:27:31 +0530

    Time: 17 ms (5.16%), Space: 41.7 MB (5.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int reverse(int x) {
+        if(x>Integer.MAX_VALUE || x==Integer.MIN_VALUE ||x==0){
+            return 0;
+        }
+        int count=0;
+        int xCopy = x;
+        boolean negativeVal=false;
+        while(xCopy>0){
+            count++;
+            xCopy=xCopy/10;
+        }
+        if(x<0){
+            negativeVal=true;
+            x=x*(-1);
+        }
+        if(negativeVal){
+            return (-1)*reverseNum(x, "");
+        }
+        return reverseNum(x, "");
+    }
+    
+    private int reverseNum(int x, String ans){
+        if(ans!="" && Long.parseLong(ans)>Integer.MAX_VALUE)        {
+            return 0;
+        }
+        if(x==0){
+            return Integer.parseInt(ans);
+        }
+        return reverseNum(x/10, ans+x%10);
+    }
+}
