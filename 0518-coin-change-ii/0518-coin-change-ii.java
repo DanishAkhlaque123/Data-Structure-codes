0903fed5f16a0c2e34e9cdc7e5fd45afc08faf06
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2024-05-05 17:04:30 +0530

    Time: 2 ms (100.00%), Space: 40.5 MB (98.83%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,16 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
-              int[] dp=new int[amount+1];
-        dp[0]=1;
-        
-        for(int coin:coins)
-        {
-            for(int i=coin;i<=amount;i++)
-            {
-                dp[i]+=dp[i-coin];
+        int[] ans=new int[amount+1];
+        ans[0]=1;
+        for(int coin : coins){
+            for(int i=coin;i<=amount;i++){
+                ans[i]+=ans[i-coin];
             }
         }
-        return dp[amount];
-    
+        return ans[ans.length-1];
     }
 }
