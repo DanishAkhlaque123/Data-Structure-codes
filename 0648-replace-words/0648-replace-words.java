acba65abeaefd80faae0f6b54dee11f024732f73
Danish Akhlaque <99094572+DanishAkhlaque123@users.noreply.github.com>
2024-06-07 09:38:14 +0530

    Time: 68 ms (27.49%), Space: 56.4 MB (18.16%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] words = sentence.split(" ");
+        Set<String> dict = new HashSet<>();
+        for(String word : dictionary){
+            dict.add(word);
+        }
+        for(int i=0;i<words.length;i++){
+            String word = words[i];
+            int len = Integer.MAX_VALUE;
+            String replacedWord="";
+            for(String root : dict){
+                if(word.startsWith(root)){
+                    if(root.length()<len){
+                        len = root.length();
+                        replacedWord = root;
+                    }
+                }
+            }
+            if(len!=Integer.MAX_VALUE){
+                words[i]=replacedWord;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String s : words){
+            sb.append(s+" ");
+        }
+        System.out.println(sb);
+        return sb.toString().trim();
+    }
+}
